4b8fe7f5dc631251491d1cf97b2c10e680a23687
heaven31415 <heaven31415@gmail.com>
2020-06-05 18:16:08 +0200

    Add a fade out animation to modal box.

--- src/javascripts/modal-box.js
+++ src/javascripts/modal-box.js
@@ -3,6 +3,7 @@ export default class ModalBox {
     this.background = document.getElementById('modal-box-background');
     this.header = document.getElementById('modal-box-header');
     this.body = document.getElementById('modal-box-body');
+    this.hidden = true;
 
     document.getElementById('modal-box-button').addEventListener('click', () => {
       this.hide();
@@ -10,12 +11,21 @@ export default class ModalBox {
   }
 
   show(header, body) {
-    this.background.style.display = 'flex';
-    this.header.textContent = header;
-    this.body.textContent = body;
+    if (this.hidden) {
+      this.header.textContent = header;
+      this.body.textContent = body;
+
+      this.background.classList.remove('fade-out');
+      this.background.classList.add('fade-in');
+      this.hidden = false;
+    }
   }
 
   hide() {
-    this.background.style.display = 'none';
+    if (!this.hidden) {
+      this.background.classList.remove('fade-in');
+      this.background.classList.add('fade-out');
+      this.hidden = true;
+    }
   }
 }
