6ad11cb91dac1cff215f888efd462848fad19836
heaven31415 <heaven31415@gmail.com>
2021-01-06 10:38:30 +0100

    Add DateTime component.

---
+++ src/components/DateTime.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ content }}</h1>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  data() {
+    return {
+      content: moment().format('D MMMM YYYY HH:mm:ss')
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.content = moment().format('D MMMM YYYY HH:mm:ss');
+    }, 1000);
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
