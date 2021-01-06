a2b27a24aeac9fa21330c5c826cdd880cbef2a23
heaven31415 <heaven31415@gmail.com>
2021-01-06 09:48:39 +0100

    Delete old files.

--- src/javascripts/error-page.js
+++
@@ -1,7 +0,0 @@
-import Datetime from './datetime';
-
-export default class ErrorPage {
-  constructor() {
-    this.datetime = new Datetime();
-  }
-}
--- src/javascripts/index.js
+++
@@ -1,7 +0,0 @@
-import App from './app';
-
-/* eslint-disable no-unused-vars */
-
-const app = new App();
-
-/* eslint-enable no-unused-vars */
