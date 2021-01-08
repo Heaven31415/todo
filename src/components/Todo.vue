7d196021f1682be0ad0e9cca41d801c362a540ba
heaven31415 <heaven31415@gmail.com>
2021-01-08 17:41:31 +0100

    Add Todo component.

---
+++ src/components/Todo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <p>{{ content }}</p>
+    <button @click="$store.commit('removeTodo', { id: id })">Delete</button>
+    <!-- <div></div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    title: String,
+    content: String,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  flex: 0 1 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px 5px;
+  background-color: white;
+  border: 1px solid #333;
+  box-shadow: 2px 2px 4px 2px rgba(30, 30, 30, 0.5);
+  overflow: auto;
+}
+
+h3 {
+  margin: 10px 0 5px;
+}
+
+p {
+  font-size: 1.2em;
+  margin: 5px;
+  padding: 5px;
+  width: 90%;
+  text-align: left;
+}
+
+button {
+  font: 1.2em "Muli", sans-serif;
+  width: 150px;
+  margin: 5px 0 10px;
+  padding: 5px;
+  background-color: rgb(205, 45, 45);
+  color: #eee;
+  border: 1px solid #333;
+  border-radius: 4px;
+  transition: background-color 0.5s ease-in;
+}
+
+button:hover {
+  cursor: pointer;
+  background-color: rgb(160, 35, 35);
+  box-shadow: 1px 1px 4px 2px rgba(30, 30, 30, 0.5);
+}
+
+button:focus {
+  box-shadow: 1px 1px 4px 2px rgba(30, 30, 30, 0.5);
+  outline: none;
+}
+</style>
