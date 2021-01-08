7f62ce33d8963958575ce54d810122906643711c
heaven31415 <heaven31415@gmail.com>
2021-01-08 17:41:38 +0100

    Add Todos component.

---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>What do you need to do?</h2>
+  <p v-if="$store.getters.todos.length === 0">You don't have to do anything!</p>
+  <div v-else>
+    <todo
+      v-for="todo in $store.getters.todos"
+      :key="todo.id"
+      :id="todo.id"
+      :title="todo.title"
+      :content="todo.content"
+    ></todo>
+  </div>
+</template>
+
+<script>
+import Todo from "./Todo.vue";
+export default {
+  components: { Todo },
+};
+</script>
+
+<style scoped>
+</style>
