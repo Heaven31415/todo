8f690a18ca72b6aa0f5e28e64bc6c8b2f911bcda
heaven31415 <heaven31415@gmail.com>
2021-01-08 06:37:16 +0100

    Improve DateTime component.

--- src/components/DateTime.vue
+++ src/components/DateTime.vue
@@ -3,22 +3,32 @@
 </template>
 
 <script>
-import moment from 'moment';
+import moment from "moment";
 
 export default {
   data() {
     return {
-      content: moment().format('D MMMM YYYY HH:mm:ss')
-    }
+      content: moment().format("D MMMM YYYY HH:mm:ss"),
+    };
   },
   mounted() {
     setInterval(() => {
-      this.content = moment().format('D MMMM YYYY HH:mm:ss');
+      this.content = moment().format("D MMMM YYYY HH:mm:ss");
     }, 1000);
-  }
+  },
 };
 </script>
 
 <style scoped>
-
+h1 {
+  width: 100%;
+  box-shadow: 0 2px 4px 2px rgba(30, 30, 30, 0.5);
+  background-color: #eee;
+  color: #333;
+  margin: 0 0 15px;
+  padding: 15px;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+}
 </style>
